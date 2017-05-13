579668ac9a33ffd08d43f74c1b03a079506220d0
arsalansaad <arsalansaad.iitkgp@gmail.com>
2017-05-13 12:35:08 +0530

    ignore case option added

--- script.py
+++ script.py
@@ -6,12 +6,13 @@ Created on Fri May 12 22:20:26 2017
 """
 
 import sys
+import argparse
 from collections import Counter
 
 def cleanser(string):
         return ''.join(e for e in string if e.isalnum())
 
-def word_count(file):
+def word_count(file,case):
     with open(file) as f:
         content = f.readlines()
     
@@ -21,14 +22,24 @@ def word_count(file):
           
     content = list(map(cleanser,content))
     content = list(filter(None,content))
-        
+    
+    if case:
+        content = [x.lower() for x in content]
     return Counter(content)
     
 def main():
-    filename = sys.argv[-1]
-    count = word_count(filename)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    parser.add_argument('-i',help='Ignores Case while counting words',action='store_true',dest='ignore')
+    args = parser.parse_args()
+    filename = args.filename
+    if args.ignore:
+        count = word_count(filename,True)
+    else:
+        count = word_count(filename,False)
     for item in count:
         print('{}, {}'.format(item,count[item]))
+    print(len(count))
 
 if __name__ == '__main__':
     main()
